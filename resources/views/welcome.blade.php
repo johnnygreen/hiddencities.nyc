cc678821db62c45b8108ebced68192bade0759fa
Johnny Green <johnnygreen@gmail.com>
2015-10-13 23:51:29 -0400

    ready to rock

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,81 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
+<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Hidden Cities</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/app.css" />
+    <script src="/js/vendor/jquery/dist/jquery.min.js"></script>
+    <script src="https://use.typekit.net/wiu1bfx.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+  </head>
+  <body class="page-home">
+    <div class="container container-header">
+      <div class="row">
+        <div class="col-sm-12">
+          <img src="/img/banner.png" />
         </div>
-    </body>
+      </div>
+      <div class="row">
+        <div class="col-sm-offset-4 col-sm-4 col-xs-offset-2 col-xs-8">
+          <h1 class="logo">
+            <div class="image-logo">
+              <img src="/img/logo.png" alt="Hiden Cities" />
+            </div>
+            <div class="image-album">
+              <img src="/img/album.png" alt="All New Friends" />
+            </div>
+          </h1>
+        </div>
+      </div>
+    </div>
+    <div class="container container-main">
+      <div class="row">
+        <div class="col-md-offset-1 col-md-5 col-sm-6">
+          <p>
+            <strong>Hidden Cities is a good excuse.</strong><br>
+            A burst of creative, cackling energy harnessed for a moment from the streets of NYC.
+          </p>
+          <p>
+            Playing those barroom dance boozy bluesy rock tunes since forever, the band has opened for national acts on legendary stages including Webster Hall and The Stone Pony, all the while writing and recording.
+          </p>
+          <p>
+            Their first EP <i>All New Friends</i> is available <a href="">HERE</a>
+          </p>
+        </div>
+        <div class="col-md-5 col-sm-6">
+          <div class="tour-dates">
+            <script type='text/javascript' src='http://widget.bandsintown.com/javascripts/bit_widget.js'></script>
+            <a href="http://www.bandsintown.com/Hidden%20Cities" class="bit-widget-initializer" data-artist="Hidden Cities">Hidden Cities Tour Dates</a>
+          </div>
+          <div class="tracks">
+            <iframe style="border: 0; width: 100%; height: 42px;" src="http://bandcamp.com/EmbeddedPlayer/track=2606667492/size=small/bgcol=ffffff/linkcol=333333/transparent=true/" seamless><a href="http://hiddencities.bandcamp.com/track/gas-on-the-fire">Gas on the Fire by Hidden Cities</a></iframe>
+            <iframe style="border: 0; width: 100%; height: 42px;" src="http://bandcamp.com/EmbeddedPlayer/track=2812767497/size=small/bgcol=ffffff/linkcol=333333/transparent=true/" seamless><a href="http://hiddencities.bandcamp.com/track/plain-jane-2">Plain Jane by Hidden Cities</a></iframe>
+            <iframe style="border: 0; width: 100%; height: 42px;" src="http://bandcamp.com/EmbeddedPlayer/track=2609923888/size=small/bgcol=ffffff/linkcol=333333/transparent=true/" seamless><a href="http://hiddencities.bandcamp.com/track/away-3">Away by Hidden Cities</a></iframe>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 text-center">
+          <ul class="social-icons">
+            <li><a href="http://instagram.com/hiddencitiesnyc"><i class="fa fa-instagram fa-2x"></i></a></li>
+            <li><a href="http://twitter.com/hiddencitiesnyc"><i class="fa fa-twitter fa-2x"></i></a></li>
+            <li><a href="http://facebook.com/hiddencitiesnyc"><i class="fa fa-facebook fa-2x"></i></a></li>
+            <li><a href="http://youtube.com/hiddencitiesnyc"><i class="fa fa-youtube fa-2x"></i></a></li>
+            <li><a href="mailto:hiddencitiesnyc@gmail.com?subject=Hello"><i class="fa fa-envelope fa-2x"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-68827346-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+  </body>
 </html>
