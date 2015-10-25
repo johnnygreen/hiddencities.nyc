801f310054dfc0ffc88ac66cce90cbed0e1ce5f0
Johnny Green <johnnygreen@gmail.com>
2015-10-25 02:57:01 -0400

    new verbiage, video up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/css/app.css" />
     <script src="/js/vendor/jquery/dist/jquery.min.js"></script>
+    <script src="/js/vendor/jquery.fitvids/jquery.fitvids.js"></script>
     <script src="https://use.typekit.net/wiu1bfx.js"></script>
     <script>try{Typekit.load({ async: true });}catch(e){}</script>
   </head>
@@ -40,12 +41,19 @@
               A burst of creative, cackling energy harnessed for a moment from the streets of NYC.
             </p>
             <p>
-              Playing those barroom dance boozy bluesy rock tunes since forever, the band has performed with national acts on legendary stages including Webster Hall and The Stone Pony.
+              Playing boozy blues barroom dance tunes, the band has performed with national acts on legendary stages including Webster Hall and The Stone Pony.
             </p>
             <p>
-              Get their first EP entitled <i>All New Friends</i> November 3rd.
+              Get their first EP entitled <i>All New Friends</i> November 6th.
             </p>
           </div>
+          <ul class="social-icons">
+            <li><a href="http://instagram.com/hiddencitiesnyc"><i class="fa fa-instagram fa-2x"></i></a></li>
+            <li><a href="http://twitter.com/hiddencitiesnyc"><i class="fa fa-twitter fa-2x"></i></a></li>
+            <li><a href="http://facebook.com/hiddencitiesnyc"><i class="fa fa-facebook fa-2x"></i></a></li>
+            <li><a href="https://www.youtube.com/watch?v=J78UrOoTAGo&feature=youtu.be"><i class="fa fa-youtube fa-2x"></i></a></li>
+            <li><a href="mailto:hiddencities@gmail.com?subject=Hello"><i class="fa fa-envelope fa-2x"></i></a></li>
+          </ul>
         </div>
         <div class="col-md-5 col-sm-6">
           <div class="tour-dates">
@@ -60,18 +68,16 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-xs-12 text-center">
-          <ul class="social-icons">
-            <li><a href="http://instagram.com/hiddencitiesnyc"><i class="fa fa-instagram fa-2x"></i></a></li>
-            <li><a href="http://twitter.com/hiddencitiesnyc"><i class="fa fa-twitter fa-2x"></i></a></li>
-            <li><a href="http://facebook.com/hiddencitiesnyc"><i class="fa fa-facebook fa-2x"></i></a></li>
-            <li><a href="https://www.youtube.com/watch?v=J78UrOoTAGo&feature=youtu.be"><i class="fa fa-youtube fa-2x"></i></a></li>
-            <li><a href="mailto:hiddencities@gmail.com?subject=Hello"><i class="fa fa-envelope fa-2x"></i></a></li>
-          </ul>
+        <div class="col-md-offset-1 col-md-10 text-center">
+          <div class="video">
+            <iframe src="https://player.vimeo.com/video/143499506" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
         </div>
       </div>
     </div>
     <script>
+      $('.video').fitVids();
+
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
