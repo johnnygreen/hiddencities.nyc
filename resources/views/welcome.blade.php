b2c924ae52b4eab3314ca9771baa45d6e985ec7a
Johnny Green <johnnygreen@gmail.com>
2019-02-12 04:03:58 -0500

    cache bust

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <title>Hidden Cities</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/css/app.css" />
+    <link rel="stylesheet" href="/css/app.css?v1" />
     <script src="/js/vendor/jquery/dist/jquery.min.js"></script>
     {{-- <script src="/js/vendor/jquery.fitvids/jquery.fitvids.js"></script> --}}
     <script src="https://use.typekit.net/wiu1bfx.js"></script>
@@ -98,7 +98,6 @@
     </div>
     <script>
       // $('.video').fitVids();
-
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
