880942d7297e275b8fe5b69ca12c4c486a0f6a06
Johnny Green <johnnygreen@gmail.com>
2019-02-12 04:02:35 -0500

    updates for new album

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,54 +27,65 @@
             <div class="image-logo">
               <img src="/img/logo.png" alt="Hiden Cities" />
             </div>
-            <div class="image-album">
+            {{-- <div class="image-album">
               <img src="/img/thestrange.png" alt="The Strange" />
-            </div>
+            </div> --}}
           </h1>
         </div>
       </div>
     </div>
     <div class="container container-main">
       <div class="row">
-        <div class="col-md-offset-1 col-md-5 col-sm-6">
+        <div class="col-md-offset-1 col-md-7 col-sm-8">
           <div class="biography">
             <p>
-              <strong>Hidden Cities is a good excuse.</strong><br>
-              A burst of creative, cackling energy harnessed for a moment from the streets of NYC.
+              <strong>Hidden Cities is the sound of crackling, creative energy, conjured from the streets of NYC.</strong>
+              From boozy bluesy bar-room rock n roll to swaggering dark dance tracks, Hidden Cities serves up the hooks & grooves to keep the party going. Based in NYC, the band performs with national acts on legendary stages including Bowery Ballroom, Webster Hall, and The Stone Pony.
             </p>
             <p>
-              Playing boozy blues barroom dance tunes, the band has performed with national acts on legendary stages including Webster Hall and The Stone Pony.
-            </p>
-            <p>
-              Get their new record <a href="http://smarturl.it/TheStrange"><img class="img-link the-strange" src="/img/thestrange.png" alt="The Strange by Hidden Cities" title="The Strange by Hidden Cities"></a>
-            </p>
-            {{-- <p>
-              Get their new single Away <a class="link" href="http://smarturl.it/hc-away">here</a>.
-            </p> --}}
-            <p>
-              Get their first record <a href="http://smarturl.it/AllNewFriends"><img class="img-link" src="/img/allnewfriends.png" alt="All New Friends by Hidden Cities" title="All New Friends by Hidden Cities"></a>
+              With a pair of EPs under their glittery belts (<a class="link" href="http://smarturl.it/AllNewFriends">All New Friends</a> and <a class="link" href="http://smarturl.it/TheStrange">The Strange</a>), the band is issuing a series of releases throughout 2019 to further solidify their place as one of NYC's sharpest outfits, starting with <a class="link" href="http://smarturl.it/BtwnFrq">Between Frequencies</a> on February 8. Fans can also catch them on the soundtrack for <a class="link" href="https://thecrew-game.ubisoft.com/the-crew-2/en-us/home/">The Crew 2</a>, out now on PS4, Xbox, PC.
             </p>
           </div>
-          <ul class="social-icons">
-            <li><a target="_blank" href="http://instagram.com/hiddencitiesnyc"><i class="fa fa-instagram fa-2x"></i></a></li>
-            <li><a target="_blank" href="http://twitter.com/hiddencitiesnyc"><i class="fa fa-twitter fa-2x"></i></a></li>
-            <li><a target="_blank" href="http://facebook.com/hiddencitiesnyc"><i class="fa fa-facebook fa-2x"></i></a></li>
-            <li><a target="_blank" href="https://www.youtube.com/watch?v=J78UrOoTAGo&feature=youtu.be"><i class="fa fa-youtube fa-2x"></i></a></li>
-            <li><a target="_blank" href="mailto:hiddencities@gmail.com?subject=Hello"><i class="fa fa-envelope fa-2x"></i></a></li>
-          </ul>
-        </div>
-        <div class="col-md-5 col-sm-6">
+          <div class="tracks">
+            <iframe src="https://open.spotify.com/embed/album/4Te6jV5EHwNSisix5HPmuG" width="100%" height="155" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            {{-- <iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/track=826315502/size=small/bgcol=ffffff/linkcol=333333/transparent=true/" seamless><a href="http://hiddencities.bandcamp.com/track/away-5">Away</a></iframe>
+            <iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/track=2812767497/size=small/bgcol=ffffff/linkcol=333333/transparent=true/" seamless><a href="http://hiddencities.bandcamp.com/track/plain-jane-2">Plain Jane</a></iframe>
+            <iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/track=1450421331/size=small/bgcol=ffffff/linkcol=333333/transparent=true/" seamless><a href="http://hiddencities.bandcamp.com/track/bottles-on-the-nightstand">Bottles on the Nightstand</a></iframe> --}}
+          </div>
           <div class="tour-dates">
             <!-- <script type='text/javascript' src='http://widget.bandsintown.com/javascripts/bit_widget.js'></script>
             <a href="http://www.bandsintown.com/Hidden%20Cities" class="bit-widget-initializer" data-artist="Hidden Cities">Hidden Cities Tour Dates</a> -->
             <script charset="utf-8" src="https://widget.bandsintown.com/main.min.js"></script>
             <a class="bit-widget-initializer" data-artist-name="Hidden Cities" data-display-local-dates="false" data-display-past-dates="true" data-auto-style="false" data-text-color="#FFFFFF" data-link-color="#000000" data-popup-background-color="rgba(0,0,0,0)" data-background-color="rgba(0,0,0,0)" data-display-limit="5" data-link-text-color="#FFFFFF"></a>
           </div>
-          <div class="tracks">
-            <iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/track=826315502/size=small/bgcol=ffffff/linkcol=333333/transparent=true/" seamless><a href="http://hiddencities.bandcamp.com/track/away-5">Away</a></iframe>
-            <iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/track=2812767497/size=small/bgcol=ffffff/linkcol=333333/transparent=true/" seamless><a href="http://hiddencities.bandcamp.com/track/plain-jane-2">Plain Jane</a></iframe>
-            <iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/track=1450421331/size=small/bgcol=ffffff/linkcol=333333/transparent=true/" seamless><a href="http://hiddencities.bandcamp.com/track/bottles-on-the-nightstand">Bottles on the Nightstand</a></iframe>
-          </div>
+          <ul class="social-icons">
+            <li><a target="_blank" href="https://open.spotify.com/artist/6OjedQanM3ftvygRKh4FrZ"><i class="fa fa-spotify fa-2x"></i></a></li>
+            <li><a target="_blank" href="http://instagram.com/hiddencitiesnyc"><i class="fa fa-instagram fa-2x"></i></a></li>
+            <li><a target="_blank" href="http://twitter.com/hiddencitiesnyc"><i class="fa fa-twitter fa-2x"></i></a></li>
+            <li><a target="_blank" href="http://facebook.com/hiddencitiesnyc"><i class="fa fa-facebook fa-2x"></i></a></li>
+            <li><a target="_blank" href="https://www.youtube.com/channel/UCVBf9VkyZdyJuX3Gf2rPLIw"><i class="fa fa-youtube fa-2x"></i></a></li>
+            <li><a target="_blank" href="mailto:hiddencities@gmail.com?subject=Hello"><i class="fa fa-envelope fa-2x"></i></a></li>
+          </ul>
+        </div>
+        <div class="col-md-3 col-sm-4">
+          <p>
+            <strong>Albums</strong>
+          </p>
+          <p>
+            <a href="http://smarturl.it/BtwnFrq">
+              <img class="img-link" src="/img/betweenfrequencies.jpg" alt="Between Frequencies by Hidden Cities" title="Between Frequencies by Hidden Cities">
+            </a>
+          </p>
+          <p>
+            <a href="http://smarturl.it/TheStrange">
+              <img class="img-link" src="/img/thestrange.jpg" alt="The Strange by Hidden Cities" title="The Strange by Hidden Cities">
+            </a>
+          </p>
+          <p>
+            <a href="http://smarturl.it/AllNewFriends">
+              <img class="img-link" src="/img/allnewfriends.jpg" alt="All New Friends by Hidden Cities" title="All New Friends by Hidden Cities">
+            </a>
+          </p>
         </div>
       </div>
       {{-- <div class="row">
